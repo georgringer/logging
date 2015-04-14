df5436ea0a68af53d828ed8ec3e41bc35d8f4437
Georg Ringer <georg.ringer@gmail.com>
2015-04-14 06:30:17 +0200

    [BUGFIX] JS for 6.2

---
+++ Resources/Public/JavaScript/logging-6.js
@@ -0,0 +1,26 @@
+jQuery(function () {
+	// apply DatePicker to all date time fields
+	//require(['TYPO3/CMS/Backend/DateTimePicker']);
+
+	if (jQuery('#log-daterange').val() == 7) {
+		jQuery('#log-date-start-container').show();
+		jQuery('#log-date-end-container').show();
+	} else {
+		jQuery('#log-date-start-container').hide();
+		jQuery('#log-date-end-container').hide();
+	}
+	jQuery('#log-daterange').change(function () {
+		if (jQuery(this).val() == 7) {
+			jQuery('#log-date-start-container').show();
+			jQuery('#log-date-end-container').show();
+		} else {
+			jQuery('#log-date-start-container').hide();
+			jQuery('#log-date-end-container').hide();
+		}
+	});
+
+
+	jQuery('.toggle-information .click').click(function () {
+		jQuery(this).parent().find('.content').toggle();
+	});
+});
