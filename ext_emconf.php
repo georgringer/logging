7e7e16c3e5d1c3466d41c70a0bb92d18b09aa361
Georg Ringer <georg.ringer@gmail.com>
2015-07-30 14:10:46 +0200

    [TASK] Update version

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,14 +2,14 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Logging',
-	'description' => '',
+	'description' => 'Logging in TYPO3 by using monolog',
 	'category' => 'backend',
 	'author' => 'Georg Ringer',
 	'author_email' => '',
 	'state' => 'beta',
 	'internal' => '',
 	'uploadfolder' => '0',
-	'createDirs' => '',
+	'createDirs' => '1.0.0',
 	'clearCacheOnLoad' => 0,
 	'version' => '',
 	'constraints' => array(
